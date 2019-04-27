7dd8d62808acdf202a57c41f893b0936852a122a
Hot Man <hotmanfromasia@gmail.com>
2019-04-27 16:22:10 +0800

    Initial commit

---
+++ app/src/main/java/trainigapp/sgti/ru/trainingappcometorussia/_MainActivity.java
@@ -0,0 +1,13 @@
+package trainigapp.sgti.ru.trainingappcometorussia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class _MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
